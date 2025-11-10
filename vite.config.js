30dd6401532bc93871afec167930541ad1e85b4d
Gustavo Motta <gustavohrmotta@gmail.com>
2025-11-10 11:08:18 +0000

    Added contact sections

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,12 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: "./src/index.html",
+        buttons: "./src/elements/buttons/index.html",
+        cards: "./src/elements/cards/index.html",
+        contact: "./src/sections/contact/index.html",
+        cta: "./src/sections/cta/index.html",
+        footers: "./src/sections/footers/index.html",
+        headers: "./src/sections/headers/index.html",
       },
     },
   },
