2f30379c44cf5edb45be6a9b61973b36959b0516
Gustavo Motta <gustavohrmotta@gmail.com>
2026-03-03 16:53:30 -0300

    fix: remove loader logic from JS entry point

--- src/_ui/scripts/index.js
+++ src/_ui/scripts/index.js
@@ -1,16 +1,6 @@
 import animateElements from "./animateElements.js";
 
 window.addEventListener("load", () => {
-  const loaders = document.querySelectorAll(".loader");
-  const loaderIndicator = document.getElementById("loader-indicator");
-  loaderIndicator?.remove();
-  loaders.forEach((el) => {
-    el.classList.add("loaded");
-    setTimeout(() => {
-      el.remove();
-    }, 800);
-  });
-
   animateElements();
 
   document.querySelectorAll("img, a").forEach((img) => {
